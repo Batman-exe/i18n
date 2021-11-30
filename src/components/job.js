06fa796860f57d01f7ff947dd5a36fc13bf02df0
Santiago tangarife Rincón <s.tangarife@uniandes.edu.co>
2021-11-30 00:03:41 -0500

    mod

--- src/components/job.js
+++ src/components/job.js
@@ -1,16 +1,31 @@
 import React from "react";
+import {FormattedDate, FormattedNumber} from 'react-intl';
 
 const Job = (props) => {
-  return (
-    <tr>
-      <th scope="row">{props.offer.id}</th>
-      <td>{props.offer.name}</td>
-      <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
-      <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
-    </tr>
-  );
+    return (
+        <tr>
+            <th scope="row">{props.offer.id}</th>
+            <td>{props.offer.name}</td>
+            <td>{props.offer.company}</td>
+            <td>{props.offer.salary} million</td>
+            <td>{props.offer.city}</td>
+            <td>
+                <FormattedDate
+                    value={new Date(props.offer.date)}
+                    year='numeric'
+                    month='long'
+                    day='numeric'
+                    weekday='long'
+                />
+            </td>
+            <td>
+                <FormattedNumber
+                    value={props.offer.visits}
+                    type='group'
+                />
+            </td>
+        </tr>
+    );
 };
 
 export default Job;
